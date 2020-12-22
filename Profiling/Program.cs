f513e5e86558fe30844b093ab4ec0815d64a5a50
Makse3351 <pugachev.vv1401s@gmail.com>
2020-12-23 01:20:12 +0200

    Add 6 lab

---
+++ Profiling/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Profiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Inside main()");
+			string numoffunc = Console.ReadLine();
+
+			int i = 0;
+
+			for (; i < 0xffffff; i++) ;
+			switch (numoffunc)
+			{
+				case "first":
+					FirstFuction();
+					break;
+				case "second":
+					SecondFunction();
+					break;
+				case "third":
+					ThirdFunction();
+					break;
+				default:
+					break;
+			}
+		}
+		static void FirstFuction()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				//process 
+				Thread.Sleep(1000);
+			}
+
+			return;
+		}
+
+		static void SecondFunction()
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				//process 
+				Thread.Sleep(1000);
+			}
+			return;
+		}
+		static void ThirdFunction()
+		{
+			for (int i = 0; i < 40; i++)
+			{
+				//process 
+				Thread.Sleep(1000);
+			}
+			return;
+		}
+	}
+}
