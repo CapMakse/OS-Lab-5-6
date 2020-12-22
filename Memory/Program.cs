6f1dbd50d41d2614f58819c3ab579056bef4c140
Makse3351 <pugachev.vv1401s@gmail.com>
2020-12-22 21:13:05 +0200

    Add 5 lab

--- Memory/Program.cs
+++ Memory/Program.cs
@@ -14,12 +14,12 @@ namespace Memory
                 stopWatch.Start();
                 for (int j = 500000000; j > 0; j--)
                 {
-                    AD[0][0]++;
-                    AD[0][0]++;
-                    //AD[1][1]+=2;
+                    //AD[0][0]++;
+                    //AD[0][0]++;
+                    AD[1][1]+=2;
                 }
 
-            AD[1][1] = AD[0][0];
+            //AD[1][1] = AD[0][0];
             stopWatch.Stop();
 
             Console.WriteLine(AD[1][1]); 
